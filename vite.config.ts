3ba22d31a8889944034c6471b6827f2c5395fd3d
ArthurP <105555096+P1nh@users.noreply.github.com>
2025-04-29 22:59:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // Para GitHub Pages, o mais simples é usar caminhos relativos:
-  base: './',
+  base: 'My-portifile
+',
   plugins: [react()],
 })
