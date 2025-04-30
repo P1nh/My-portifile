bad2eb89608abc6d5dcd424f68fc42a303c15d74
ArthurP <105555096+P1nh@users.noreply.github.com>
2025-04-29 22:58:16 -0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  // Para GitHub Pages, o mais simples é usar caminhos relativos:
+  base: './',
+  plugins: [react()],
+})/// <reference types="vite/client" />
