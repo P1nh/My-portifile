9babe5f853f42580fe68bc2df61c40878c2ee4e4
ArthurP <105555096+P1nh@users.noreply.github.com>
2025-04-30 12:36:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// substitua "meu-portfolio" pelo slug exato do seu repo no GitHub
 export default defineConfig({
-  // Para GitHub Pages, o mais simples é usar caminhos relativos:
-  base: './',
+  base: '/My-portifile/',
   plugins: [react()],
 })
