1200974cc40670db1f0c3febbce004cb8da52c34
ArthurP <105555096+P1nh@users.noreply.github.com>
2025-04-30 13:38:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// substitua "meu-portfolio" pelo slug exato do seu repo no GitHub
 export default defineConfig({
   base: '/My-portifile/',
+  build: {
+    outDir: 'docs',    // <<-- aqui
+  },
   plugins: [react()],
 })
